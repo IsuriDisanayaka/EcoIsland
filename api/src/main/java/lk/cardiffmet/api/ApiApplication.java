ff531cbdafaa2e14663096331d35ffee7460a78c
IsuriDisanayaka <isuriumeshika1@gmail.com>
2023-09-24 13:47:35 +0530

    #1Feat:dockerize api

--- api/src/main/java/lk/cardiffmet/api/ApiApplication.java
+++ api/src/main/java/lk/cardiffmet/api/ApiApplication.java
@@ -2,12 +2,20 @@ package lk.cardiffmet.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Dockerizing new Spring Boot Application";
 	}
 
 }
